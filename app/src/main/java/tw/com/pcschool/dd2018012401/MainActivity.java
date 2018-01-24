8eb4ec27aedd78b0974085aed52e3491e13f3364
Teacher <shaojung@gmail.com>
2018-01-24 10:13:19 +0800

    20180124001

--- app/src/main/java/tw/com/pcschool/dd2018012401/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018012401/MainActivity.java
@@ -1,6 +1,9 @@
 package tw.com.pcschool.dd2018012401;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +15,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +42,35 @@ public class MainActivity extends AppCompatActivity {
             Log.d("FILE", "nf 不存在");
         }
 
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] {WRITE_EXTERNAL_STORAGE,
+                            READ_EXTERNAL_STORAGE},
+                    123
+            );
+        }
+        else
+        {
+            readFile();
+        }
+
+
+
+    }
+    public void click2(View v)
+    {
+        File f = getExternalFilesDir("data");
+        Log.d("FILE", f.getAbsolutePath());
+    }
+    private void readFile()
+    {
+        File f = Environment.getExternalStorageDirectory();
+
+        File nf = new File(f, "DCIM" + File.separator + "myfile.txt");
         try {
             FileReader fr = new FileReader(nf);
             BufferedReader br = new BufferedReader(fr);
@@ -45,11 +81,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-    public void click2(View v)
-    {
-        File f = getExternalFilesDir("data");
-        Log.d("FILE", f.getAbsolutePath());
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 123)
+        {
+            if (grantResults.length > 0
+                    && grantResults[0] == PERMISSION_GRANTED) {
+                //取得權限，進行檔案存取
+                readFile();
+            } else {
+                //使用者拒絕權限，停用檔案存取功能
+            }
+            return;
+        }
     }
 }
